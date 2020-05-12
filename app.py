1f4e8bbb370b556aefb81ba7c623402c5d186bcc
Mohammed-benotmane <mohammedbenotmanetom@gmail.com>
2020-05-12 04:18:09 +0200

    add project

---
+++ app.py
@@ -0,0 +1,28 @@
+import os
+from flask import Flask
+from models import setup_db
+from flask_cors import CORS
+
+def create_app(test_config=None):
+
+    app = Flask(__name__)
+    setup_db(app)
+    CORS(app)
+
+    @app.route('/')
+    def get_greeting():
+        excited = os.environ['EXCITED']
+        greeting = "toz toz" 
+        if excited == 'true': greeting = greeting + "!!!!!"
+        return greeting
+
+    @app.route('/coolkids')
+    def be_cool():
+        return "Be cool, man, be coooool! You're almost a FSND grad!"
+
+    return app
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run()
---
+++ models.py
@@ -0,0 +1,81 @@
+from sqlalchemy import Column, String, create_engine,Integer,ForeignKey
+from flask_sqlalchemy import SQLAlchemy
+import json
+import os
+database_name="finalproject"
+database_path = os.environ.get('DATABASE_URL',"postgres://{}:{}@{}/{}".format('postgres', '','localhost:5432', database_name))
+
+db = SQLAlchemy()
+
+'''
+setup_db(app)
+    binds a flask application and a SQLAlchemy service
+'''
+def setup_db(app, database_path=database_path):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    db.create_all()
+
+
+'''
+Pharmacy
+'''
+class Pharmacy(db.Model):  
+  __tablename__ = 'Pharmacy'
+
+  id = Column(Integer, primary_key=True)
+  name = Column(String)
+  location = Column(String)
+  phoneNumber = Column(Integer)
+
+  def __init__(self, name, location, phoneNumber):
+    self.name = name
+    self.location = location
+    self.phoneNumber = phoneNumber
+
+  def format(self):
+    return {
+      'id': self.id,
+      'name': self.name,
+      'location': self.location,
+      'phoneNumber': self.phoneNumber
+      }
+
+class Medication(db.Model):  
+  __tablename__ = 'Medication'
+
+  id = Column(Integer, primary_key=True)
+  medicationName = Column(String)
+  price = Column(Integer)
+
+  def __init__(self, medicationName, price):
+    self.medicationName = medicationName
+    self.price = price
+
+  def format(self):
+    return {
+      'id': self.id,
+      'medicationName': self.medicationName,
+      'price': self.price
+      }
+
+class MedicationPharmacy(db.Model):  
+  __tablename__ = 'MedicationPharmacy'
+
+  pharmacyId = Column(Integer,ForeignKey('Pharmacy.id'), primary_key=True)
+  medicationId = Column(Integer,ForeignKey('Medication.id'), primary_key=True)
+  quantity = Column(Integer)
+
+  def __init__(self, pharmacyId, medicationId, quantity):
+    self.pharmacyId = pharmacyId
+    self.medicationId = medicationId
+    self.quantity = quantity
+
+  def format(self):
+    return {
+      'pharmacyId': self.pharmacyId,
+      'medicationId': self.medicationId,
+      'quantity': self.quantity
+      }
