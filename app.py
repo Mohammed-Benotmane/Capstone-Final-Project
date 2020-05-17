4b15fd51f0a4ee698cbd336a476619916e97f72f
Mohammed-benotmane <mohammedbenotmanetom@gmail.com>
2020-05-17 08:25:18 +0200

    update project

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask,jsonify,request,abort
 from models import setup_db,Medication,MedicationPharmacy,Pharmacy
 from flask_cors import CORS
+from auth.auth import AuthError, requires_auth
 
 def create_app(test_config=None):
 
@@ -10,7 +11,8 @@ def create_app(test_config=None):
     CORS(app)
 
     @app.route('/medications')
-    def get_medication():
+    @requires_auth('get:medications')
+    def get_medication(token):
         medications = Medication.query.all()
         formatted_medications = [medication.format() for medication in medications]
         return jsonify({
@@ -20,7 +22,8 @@ def create_app(test_config=None):
         })
 
     @app.route('/disponibilities')
-    def get_disponibilities():
+    @requires_auth('get:disponibilities')
+    def get_disponibilities(token):
         disponibilities = MedicationPharmacy.query.all()
         formatted_disponibilities = [disponibility.format() for disponibility in disponibilities]
         return jsonify({
@@ -31,7 +34,8 @@ def create_app(test_config=None):
 
 
     @app.route('/pharmacies')
-    def get_pharmacies():
+    @requires_auth('get:pharmacies')
+    def get_pharmacies(token):
         pharmacies = Pharmacy.query.all()
         formatted_pharmacies = [pharmacy.format() for pharmacy in pharmacies]
         return jsonify({
@@ -41,7 +45,8 @@ def create_app(test_config=None):
         })
 
     @app.route('/pharmacies/<int:pharmacy_id>',methods=['DELETE'])
-    def delete_pharmacy(pharmacy_id):
+    @requires_auth('delete:pharmacies')
+    def delete_pharmacy(token, pharmacy_id):
         pharmacy = Pharmacy.query.filter(Pharmacy.id == pharmacy_id).one_or_none()
         if pharmacy is None:
             abort(404)
@@ -54,7 +59,8 @@ def create_app(test_config=None):
         })
 
     @app.route('/disponibilities/<int:disponibility_id>',methods=['DELETE'])
-    def delete_disponibility(disponibility_id):
+    @requires_auth('delete:disponibilities')
+    def delete_disponibility(token, disponibility_id):
         disponibility = MedicationPharmacy.query.filter(MedicationPharmacy.id == disponibility_id).one_or_none()
         if disponibility is None:
             abort(404)
@@ -67,7 +73,8 @@ def create_app(test_config=None):
         })
 
     @app.route('/medications/<int:medication_id>',methods=['DELETE'])
-    def delete_medication(medication_id):
+    @requires_auth('delete:medications')
+    def delete_medication(token,medication_id):
         medication = Medication.query.filter(Medication.id == medication_id).one_or_none()
         if medication is None:
             abort(404)
@@ -80,7 +87,8 @@ def create_app(test_config=None):
         })
 
     @app.route('/pharmacies',methods=['POST'])
-    def create_pharmacies():
+    @requires_auth('post:pharmacies')
+    def create_pharmacies(token):
         body = request.get_json()
         new_name = body.get("name",None)
         new_location = body.get("location",None)
@@ -99,7 +107,8 @@ def create_app(test_config=None):
         })
 
     @app.route('/disponibilities',methods=['POST'])
-    def create_disponibilities():
+    @requires_auth('post:disponibilities')
+    def create_disponibilities(token):
         body = request.get_json()
         new_pharmacyId = body.get("pharmacyId",None)
         new_medicationId = body.get("medicationId",None)
@@ -118,7 +127,8 @@ def create_app(test_config=None):
         })
 
     @app.route('/medications',methods=['POST'])
-    def create_medications():
+    @requires_auth('post:medications')
+    def create_medications(token):
         body = request.get_json()
         new_medicationName = body.get("medicationName",None)
         new_price = body.get("price",None)
@@ -136,7 +146,8 @@ def create_app(test_config=None):
         })
 
     @app.route("/pharmacies/<pharmacy_id>", methods=['PATCH'])
-    def patch_pharmacies(pharmacy_id):
+    @requires_auth('patch:pharmacies')
+    def patch_pharmacies(token,pharmacy_id):
         body = request.get_json()
         pharmacy = Pharmacy.query.get(pharmacy_id)
         if body.get("name"):
@@ -152,7 +163,8 @@ def create_app(test_config=None):
         })
 
     @app.route("/disponibilities/<disponibility_id>", methods=['PATCH'])
-    def patch_disponibility(disponibility_id):
+    @requires_auth('patch:disponibilities')
+    def patch_disponibility(token,disponibility_id):
         body = request.get_json()
         disponibility = MedicationPharmacy.query.get(disponibility_id)
         if body.get("pharmacyId"):
@@ -168,7 +180,8 @@ def create_app(test_config=None):
         })
 
     @app.route("/medications/<medication_id>", methods=['PATCH'])
-    def patch_medications(medication_id):
+    @requires_auth('patch:medications')
+    def patch_medications(token, medication_id):
         body = request.get_json()
         medication = Medication.query.get(medication_id)
         if body.get("medicationName"):
@@ -180,6 +193,38 @@ def create_app(test_config=None):
             "success": True,
             "medication": medication.format()
         })
+    
+    @app.errorhandler(400)
+    def bad_request(error):
+        return jsonify({
+            "success": False,
+            "error": 400,
+            "message": "Bad Request"
+        }), 400
+
+    @app.errorhandler(404)
+    def not_found(error):
+        return jsonify({
+            "success": False,
+            "error": 404,
+            "message": "resource not found"
+        }), 404
+
+    @app.errorhandler(422)
+    def unprocessable(error):
+        return jsonify({
+            "success": False,
+            "error": 422,
+            "message": "unprocessable"
+        }), 422
+    
+    @app.errorhandler(500)
+    def internal_server_error(error):
+        return jsonify({
+            "success": False,
+            "error": 500,
+            "message": "500 Internal Server Error” "
+        }), 500
     
 
 
---
+++ capstonetest.py
@@ -0,0 +1,71 @@
+import os
+import unittest
+import json
+from flask_sqlalchemy import SQLAlchemy
+from models import setup_db, Pharmacy, Medication, MedicationPharmacy
+from app import create_app
+
+class TestAppWrapper(object):
+
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        environ['HTTP_AUTHORIZATION'] = 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik5qRkNRemt4TVRGQ1JUVTVOalk1TnpjNVJUQkRSa0ZGUXpVMU4wWkRPRVZFUkRaQ09VSXhNUSJ9.eyJpc3MiOiJodHRwczovL21vaGFtbWVkYmVub3RtYW5lLmF1dGgwLmNvbS8iLCJzdWIiOiJnb29nbGUtb2F1dGgyfDEwMTkxMjgwNjg0MjY5NDExNTE0NyIsImF1ZCI6WyJjYXBzdG9uZSIsImh0dHBzOi8vbW9oYW1tZWRiZW5vdG1hbmUuYXV0aDAuY29tL3VzZXJpbmZvIl0sImlhdCI6MTU4OTY5NjM3NCwiZXhwIjoxNTg5NzAzNTc0LCJhenAiOiJUdmNjTWNINjZTUktkNW56aUw0QzkxVTdaN1ZlOXNhdSIsInNjb3BlIjoib3BlbmlkIHByb2ZpbGUgZW1haWwiLCJwZXJtaXNzaW9ucyI6WyJkZWxldGU6ZGlzcG9uaWJpbGl0aWVzIiwiZGVsZXRlOm1lZGljYXRpb25zIiwiZGVsZXRlOnBoYXJtYWNpZXMiLCJnZXQ6ZGlzcG9uaWJpbGl0aWVzIiwiZ2V0Om1lZGljYXRpb25zIiwiZ2V0OnBoYXJtYWNpZXMiLCJwYXRjaDpkaXNwb25pYmlsaXRpZXMiLCJwYXRjaDptZWRpY2F0aW9ucyIsInBhdGNoOnBoYXJtYWNpZXMiLCJwb3N0OmRpc3BvbmliaWxpdGllcyIsInBvc3Q6bWVkaWNhdGlvbnMiLCJwb3N0OnBoYXJtYWNpZXMiXX0.bgWOt9e9IY2Q4NL5cThti6M1p-EXQCKIbxsq-7MvVtFHVUhinkCz7t06vWm-gaXIreoHPQnH-AEIyhMmB85ROLAL3PnW-FgqzvyniGxrxsRsizawxfQScLmeogWA9FhfJF4-sX82VsD-AvhHUKL3_6IMjUUTugk58nWOE2fvgJA2Y--sa5Oj9teV8WAe3niHz6bXWRgKgVW-I85lNfBTW8bmudBhNJu3I_SNtyKn88Qm7HoNxMQrj6IAKKrcPI7murzzoFgT0b5eFxOc5F6ZNpd8cyilpIN9Gy3e6I-JGlYIcjOge-dcNIC2FUIE2gWy74n0ZH7cZfqaSkUJaoggJA'
+        return self.app(environ, start_response)
+
+
+class CapstoneTestCase(unittest.TestCase):
+   
+    def setUp(self):
+        self.app = create_app()
+        self.app.wsgi_app = TestAppWrapper(self.app.wsgi_app)
+        self.client = self.app.test_client
+        self.database_name = "capstone_test"
+        self.database_path = os.environ.get('DATABASE_URL',"postgres://{}:{}@{}/{}".format('postgres', '','localhost:5432', self.database_name))
+        setup_db(self.app, self.database_path)
+
+        self.new_pharmacy = {
+            'question':'where is situated Algeria?',
+            'answer':'North Africa',
+            'difficulty':3,
+            'category': 3
+        }
+        with self.app.app_context():
+            self.db = SQLAlchemy()
+            self.db.init_app(self.app)
+            # create all tables
+            self.db.create_all()
+
+    def tearDown(self):
+        """Executed after reach test"""
+        pass
+
+    def test_get_pharmacies(self):
+        res = self.client().get('/pharmacies')
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code,200)
+        self.assertEqual(data['success'],True)
+        self.assertTrue(data['pharmacies'])
+        self.assertTrue(data['total_pharmacies'])
+
+    def test_get_medications(self):
+        res = self.client().get('/medications')
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code,200)
+        self.assertEqual(data['success'],True)
+        self.assertTrue(data['medications'])
+        self.assertTrue(data['total_medications'])
+
+    
+
+
+
+
+        
+
+
+# Make the tests conveniently executable
+if __name__ == "__main__":
+    unittest.main()
+
