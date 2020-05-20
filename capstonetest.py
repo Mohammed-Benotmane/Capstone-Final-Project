2a2562e035d961bafa742b37c48d93872b4d1d0c
Mohammed-benotmane <mohammedbenotmanetom@gmail.com>
2020-05-20 17:28:52 +0200

    adding success unittest

--- capstonetest.py
+++ capstonetest.py
@@ -11,7 +11,7 @@ class TestAppWrapper(object):
         self.app = app
 
     def __call__(self, environ, start_response):
-        environ['HTTP_AUTHORIZATION'] = 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik5qRkNRemt4TVRGQ1JUVTVOalk1TnpjNVJUQkRSa0ZGUXpVMU4wWkRPRVZFUkRaQ09VSXhNUSJ9.eyJpc3MiOiJodHRwczovL21vaGFtbWVkYmVub3RtYW5lLmF1dGgwLmNvbS8iLCJzdWIiOiJnb29nbGUtb2F1dGgyfDEwMTkxMjgwNjg0MjY5NDExNTE0NyIsImF1ZCI6WyJjYXBzdG9uZSIsImh0dHBzOi8vbW9oYW1tZWRiZW5vdG1hbmUuYXV0aDAuY29tL3VzZXJpbmZvIl0sImlhdCI6MTU4OTc4NTEyMywiZXhwIjoxNTg5NzkyMzIzLCJhenAiOiJUdmNjTWNINjZTUktkNW56aUw0QzkxVTdaN1ZlOXNhdSIsInNjb3BlIjoib3BlbmlkIHByb2ZpbGUgZW1haWwiLCJwZXJtaXNzaW9ucyI6WyJkZWxldGU6ZGlzcG9uaWJpbGl0aWVzIiwiZGVsZXRlOm1lZGljYXRpb25zIiwiZGVsZXRlOnBoYXJtYWNpZXMiLCJnZXQ6ZGlzcG9uaWJpbGl0aWVzIiwiZ2V0Om1lZGljYXRpb25zIiwiZ2V0OnBoYXJtYWNpZXMiLCJwYXRjaDpkaXNwb25pYmlsaXRpZXMiLCJwYXRjaDptZWRpY2F0aW9ucyIsInBhdGNoOnBoYXJtYWNpZXMiLCJwb3N0OmRpc3BvbmliaWxpdGllcyIsInBvc3Q6bWVkaWNhdGlvbnMiLCJwb3N0OnBoYXJtYWNpZXMiXX0.Gr6T8flFdqYP93L-l56ZgFNpb9cA69YL54Zx8wp9WbL-V-SVnLQQ20CyPOZJtNPRVzcj6_NYNvtx-XEFc8JFVdPBa_xOODdD12RcmJHU1Bc35TIqTtKQ3nS1Rp_YtqsUXK0pid4xDbPIL_J0woyvGtAS3AH73KzZe-wrS07JHDNwyTyFFclnUtR_OISzn9YUGT3w5tq5y8RElOvzL4iM17ZpR5-nXyn74ByA5OS8dnp5dlccPXBDfmpg5L3KAMOPZmYU-H6uQDatS8xxZ2QTt-toRZ6ivGToeUmWul2FDgCzN6KXpRQPEe8ENpvNfzzuC1TVXA-UgXPji029DkXFRg'
+        environ['HTTP_AUTHORIZATION'] = 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik5qRkNRemt4TVRGQ1JUVTVOalk1TnpjNVJUQkRSa0ZGUXpVMU4wWkRPRVZFUkRaQ09VSXhNUSJ9.eyJpc3MiOiJodHRwczovL21vaGFtbWVkYmVub3RtYW5lLmF1dGgwLmNvbS8iLCJzdWIiOiJnb29nbGUtb2F1dGgyfDEwMTkxMjgwNjg0MjY5NDExNTE0NyIsImF1ZCI6WyJjYXBzdG9uZSIsImh0dHBzOi8vbW9oYW1tZWRiZW5vdG1hbmUuYXV0aDAuY29tL3VzZXJpbmZvIl0sImlhdCI6MTU4OTk4NjY0MiwiZXhwIjoxNTg5OTkzODQyLCJhenAiOiJUdmNjTWNINjZTUktkNW56aUw0QzkxVTdaN1ZlOXNhdSIsInNjb3BlIjoib3BlbmlkIHByb2ZpbGUgZW1haWwiLCJwZXJtaXNzaW9ucyI6WyJkZWxldGU6ZGlzcG9uaWJpbGl0aWVzIiwiZGVsZXRlOm1lZGljYXRpb25zIiwiZGVsZXRlOnBoYXJtYWNpZXMiLCJnZXQ6ZGlzcG9uaWJpbGl0aWVzIiwiZ2V0Om1lZGljYXRpb25zIiwiZ2V0OnBoYXJtYWNpZXMiLCJwYXRjaDpkaXNwb25pYmlsaXRpZXMiLCJwYXRjaDptZWRpY2F0aW9ucyIsInBhdGNoOnBoYXJtYWNpZXMiLCJwb3N0OmRpc3BvbmliaWxpdGllcyIsInBvc3Q6bWVkaWNhdGlvbnMiLCJwb3N0OnBoYXJtYWNpZXMiXX0.tgtPWbnuxxaAb6IgO0rcDJ-eYQhMBdJkNQVqEW1DMxQ3CrF6VGC4Ms9tRbB0LYTG_sDn_TykNMI44KGouQ3eZ2FlSLIuYN052--APkF23rquD3bt49Nj53Vyxe_BmaA5IIHz4154cpjMtP54ouRm6WEwX28nMAXNknnu_ZLhg3fSjjsEFgF-vgwfBSmeB0enQRh89FM2hXsa9BQwvEuTR02XuaSwpjDqo9hAkz6sykGGk1oDJtSVAC70GlX6Lb63TgnXN5RtN9ZgO8R7QxO6duJO5AYwkUz-AIXMdJc4swz2w-MGPv4TiS1-1c0sqg6WL2mStMEI4p5vVi1Vk8AccQ'
         return self.app(environ, start_response)
 
 
@@ -35,11 +35,7 @@ class CapstoneTestCase(unittest.TestCase):
             'medicationName':'Doliprane',
             'price':'120'
         }
-        self.new_disponibility = {
-            'pharmacyId':1,
-            'medicationId':2,
-            'quantity':15
-        }
+        
         with self.app.app_context():
             self.db = SQLAlchemy()
             self.db.init_app(self.app)
@@ -49,6 +45,34 @@ class CapstoneTestCase(unittest.TestCase):
     def tearDown(self):
         """Executed after reach test"""
         pass
+
+    def test_create_pharmacies(self):
+        res = self.client().post('/pharmacies',json=self.new_pharmacy)
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code,200)
+        self.assertEqual(data['success'],True)
+        self.assertTrue(data['pharmacies'])
+        self.assertTrue(data['total_pharmacies'])
+
+    def test_create_medications(self):
+        res = self.client().post('/medications',json=self.new_medication)
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code,200)
+        self.assertEqual(data['success'],True)
+        self.assertTrue(data['medications'])
+        self.assertTrue(data['total_medications'])
+    
+    def test_create_disponibilities(self):
+        res = self.client().post('/disponibilities',json={
+            'pharmacyId':Pharmacy.query.first().id,
+            'medicationId':Medication.query.first().id,
+            'quantity':15
+        })
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code,200)
+        self.assertEqual(data['success'],True)
+        self.assertTrue(data['disponibilities'])
+        self.assertTrue(data['total_disponibilities'])
     
     def test_get_pharmacies(self):
         res = self.client().get('/pharmacies')
@@ -74,29 +98,35 @@ class CapstoneTestCase(unittest.TestCase):
         self.assertTrue(data['disponibilities'])
         self.assertTrue(data['total_disponibilities'])
 
-    def test_create_pharmacies(self):
-        res = self.client().post('/pharmacies',json=self.new_pharmacy)
+    def test_update_pharmacies(self):
+        id=Pharmacy.query.first().id
+        res = self.client().patch('/pharmacies/'+str(id),json={
+            'name':'test'
+        })
         data = json.loads(res.data)
         self.assertEqual(res.status_code,200)
         self.assertEqual(data['success'],True)
-        self.assertTrue(data['pharmacies'])
-        self.assertTrue(data['total_pharmacies'])
+        self.assertTrue(data['pharmacy'])
 
-    def test_create_medications(self):
-        res = self.client().post('/medications',json=self.new_medication)
+    def test_update_medication(self):
+        id=Medication.query.first().id
+        res = self.client().patch('/medications/'+str(id),json={
+            'price':100
+        })
         data = json.loads(res.data)
         self.assertEqual(res.status_code,200)
         self.assertEqual(data['success'],True)
-        self.assertTrue(data['medications'])
-        self.assertTrue(data['total_medications'])
-    
-    def test_create_disponibilities(self):
-        res = self.client().post('/disponibilities',json=self.new_disponibility)
+        self.assertTrue(data['medication'])
+
+    def test_update_disponibility(self):
+        id=MedicationPharmacy.query.first().id
+        res = self.client().patch('/disponibilities/'+str(id),json={
+            'quantity':100
+        })
         data = json.loads(res.data)
         self.assertEqual(res.status_code,200)
         self.assertEqual(data['success'],True)
-        self.assertTrue(data['disponibilities'])
-        self.assertTrue(data['total_disponibilities'])
+        self.assertTrue(data['disponibility'])
 
     def test_delete_disponibility(self):
         disponibility = MedicationPharmacy.query.first()
@@ -106,6 +136,43 @@ class CapstoneTestCase(unittest.TestCase):
         self.assertEqual(data['success'],True)
         self.assertEqual(data['deleted'],disponibility.id)
         self.assertTrue(data['disponibilities'])
+
+    def test_delete_pharmacy(self):
+        pharmacy = Pharmacy.query.first()
+        res = self.client().delete('/pharmacies/'+str(pharmacy.id))
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code,200)
+        self.assertEqual(data['success'],True)
+        self.assertEqual(data['deleted'],pharmacy.id)
+        self.assertTrue(data['pharmacies'])
+
+    def test_delete_medication(self):
+        medication = Medication.query.first()
+        res = self.client().delete('/medications/'+str(medication.id))
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code,200)
+        self.assertEqual(data['success'],True)
+        self.assertEqual(data['deleted'],medication.id)
+        self.assertTrue(data['medications'])
+
+'''
+    def test_delete_disponibility(self):
+        disponibility = MedicationPharmacy.query.first()
+        res = self.client().delete('/disponibilities/'+str(disponibility.id))
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code,200)
+        self.assertEqual(data['success'],True)
+        self.assertEqual(data['deleted'],disponibility.id)
+        self.assertTrue(data['disponibilities'])
+
+    def test_update_pharmacies(self):
+        res = self.client().patch('/pharmacies/1',json={
+            'name':'test'
+        })
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code,200)
+        self.assertEqual(data['success'],True)
+        self.assertTrue(data['pharmacy'])
 
     def test_delete_pharmacy(self):
         pharmacy = Pharmacy.query.first()
@@ -119,6 +186,8 @@ class CapstoneTestCase(unittest.TestCase):
         self.assertEqual(data['deleted'],pharmacy.id)
         self.assertTrue(data['pharmacies'])
         self.assertEqual(pharmacy,None)
+    
+'''
     
 
     
