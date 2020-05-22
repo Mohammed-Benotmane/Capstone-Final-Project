0018fec946f5126c85daad62e20c8ada38b701de
Mohammed-benotmane <mohammedbenotmanetom@gmail.com>
2020-05-22 17:30:51 +0200

    update project

--- auth/auth.py
+++ auth/auth.py
@@ -25,25 +25,32 @@ class AuthError(Exception):
 # Auth Header
 
 def get_token_auth_header():
-    auth = request.headers.get("Authorization", None)
+    auth = request.headers.get('Authorization', None)
     if not auth:
-          raise AuthError({"code": "authorization_header_missing",
-                            "description":
-                            "Authorization header is expected"}, 401)
+        raise AuthError({
+            'code': 'authorization_header_missing',
+            'description': 'Authorization header is expected.'
+        }, 401)
+
     parts = auth.split()
-    if parts[0].lower() != "bearer":
-          raise AuthError({"code": "invalid_header",
-                          "description":
-                          "Authorization header must start with"
-                          " Bearer"}, 401)
+    if parts[0].lower() != 'bearer':
+        raise AuthError({
+            'code': 'invalid_header',
+            'description': 'Authorization header must start with "Bearer".'
+        }, 401)
+
     elif len(parts) == 1:
-          raise AuthError({"code": "invalid_header",
-                        "description": "Token not found"}, 401)
+        raise AuthError({
+            'code': 'invalid_header',
+            'description': 'Token not found.'
+        }, 401)
+
     elif len(parts) > 2:
-          raise AuthError({"code": "invalid_header",
-                        "description":
-                        "Authorization header must be"
-                        " Bearer token"}, 401)
+        raise AuthError({
+            'code': 'invalid_header',
+            'description': 'Authorization header must be bearer token.'
+        }, 401)
+
     token = parts[1]
     return token
 
