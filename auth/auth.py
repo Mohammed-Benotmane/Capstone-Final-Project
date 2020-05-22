5426d50e4eef8a0bf1c1b2116e1c638dd7a603be
Mohammed-benotmane <mohammedbenotmanetom@gmail.com>
2020-05-22 12:52:44 +0200

    update project

--- auth/auth.py
+++ auth/auth.py
@@ -1,5 +1,5 @@
 import json
-from flask import request, _request_ctx_stack,abort
+from flask import request, _request_ctx_stack, abort
 from functools import wraps
 from jose import jwt
 from urllib.request import urlopen
@@ -9,46 +9,41 @@ AUTH0_DOMAIN = 'mohammedbenotmane.auth0.com'
 ALGORITHMS = ['RS256']
 API_AUDIENCE = 'capstone'
 
-## AuthError Exception
+# AuthError Exception
 '''
 AuthError Exception
 A standardized way to communicate auth failure modes
 '''
+
+
 class AuthError(Exception):
     def __init__(self, error, status_code):
         self.error = error
         self.status_code = status_code
 
 
-## Auth Header
+# Auth Header
 
 def get_token_auth_header():
-    auth = request.headers.get('Authorization', None)
+    auth = request.headers.get("Authorization", None)
     if not auth:
-        raise AuthError({
-            'code': 'authorization_header_missing',
-            'description': 'Authorization header is expected.'
-        }, 401)
-
+          raise AuthError({"code": "authorization_header_missing",
+                            "description":
+                            "Authorization header is expected"}, 401)
     parts = auth.split()
-    if parts[0].lower() != 'bearer':
-        raise AuthError({
-            'code': 'invalid_header',
-            'description': 'Authorization header must start with "Bearer".'
-        }, 401)
-
+    if parts[0].lower() != "bearer":
+          raise AuthError({"code": "invalid_header",
+                          "description":
+                          "Authorization header must start with"
+                          " Bearer"}, 401)
     elif len(parts) == 1:
-        raise AuthError({
-            'code': 'invalid_header',
-            'description': 'Token not found.'
-        }, 401)
-
+          raise AuthError({"code": "invalid_header",
+                        "description": "Token not found"}, 401)
     elif len(parts) > 2:
-        raise AuthError({
-            'code': 'invalid_header',
-            'description': 'Authorization header must be bearer token.'
-        }, 401)
-
+          raise AuthError({"code": "invalid_header",
+                        "description":
+                        "Authorization header must be"
+                        " Bearer token"}, 401)
     token = parts[1]
     return token
 
